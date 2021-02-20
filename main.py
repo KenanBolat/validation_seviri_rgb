c5c9dd30f0a6f78c8fb8ed8e3d2fb8d3ae5f7827
KenanBolat <kenan23@gmail.com>
2021-02-21 00:34:58 +0300

    Uncomment natural_color solution

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ process_path = r"C:\Users\knn\Data\MSG"
 files = [os.path.join(process_path, row) for row in glob.glob1(process_path, "W_XX*"+date_+hour_+"*.nc")]
 
 print(files)
-# scn = Scene(reader="seviri_l1b_nc", filenames=[file_])
-# pprint.pprint(scn.available_composite_names())
-# scn.load(['natural_color'], calibrations=[ 'radiance'])
-# scn.show("natural_color")
+scn = Scene(reader="seviri_l1b_nc", filenames=files)
+pprint.pprint(scn.available_composite_names())
+scn.load(['natural_color'], calibrations=[ 'radiance'])
+scn.show("natural_color")
 # scn.load(['day_microphysics_winter'], calibrations=['radiance'])
 # scn.show("day_microphysics_winter")
 
